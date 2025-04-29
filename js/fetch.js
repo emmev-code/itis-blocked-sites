2cd9a52466aa98261ea980fa40f562500b2a8868
Vichingo455 <vichingo455@gmail.com>
2025-04-29 14:34:28 +0200

    Little update

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,5 @@
-fetch('https://gist.githubusercontent.com/usefulstuffs/930ca4c5442fcccbb3554276f453b35b/raw/blocklist.json')
+function fetchData() {
+  fetch('https://gist.githubusercontent.com/usefulstuffs/930ca4c5442fcccbb3554276f453b35b/raw/blocklist.json')
       .then(response => {
         if (!response.ok) {
           throw new Error('Errore nel caricamento del file JSON');
@@ -7,19 +8,23 @@ fetch('https://gist.githubusercontent.com/usefulstuffs/930ca4c5442fcccbb3554276f
       })
       .then(siti => {
         const tbody = document.querySelector('#tabella-siti tbody');
+        tbody.innerHTML = ""; // Inizializzazione vuota per non creare problemi col timer
         siti.forEach(sito => {
           const statoTesto = sito.stato || "Sconosciuto";
           const note = sito.note || "";
-
+          // Definisce la colorazione dei vari stati
           let classeStato = "";
           const statoLower = statoTesto.toLowerCase();
-
-          if (statoLower.includes("sbloccato")) {
+          if (statoLower == "non bloccato") {
+            classeStato = "sbloccato";
+          } else if (statoLower.includes("sbloccato")) {
             classeStato = "sbloccato";
           } else if (statoLower.includes("bloccato")) {
-            classeStato = statoLower.includes("parzial") ? "parziale" : "bloccato";
+            classeStato = statoLower.includes("parzialmente") ? "parziale" : "bloccato";
+          } else {
+            classeStato = "unknown";
           }
-
+          // Inserisce i dati ottenuti in una riga della tabella
           const riga = document.createElement('tr');
           riga.innerHTML = `
             <td>${sito.nome}</td>
@@ -32,5 +37,8 @@ fetch('https://gist.githubusercontent.com/usefulstuffs/930ca4c5442fcccbb3554276f
       })
       .catch(error => {
         alert('Errore durante il caricamento dei dati, per favore riprova o riporta il problema su GitHub: https://github.com/emmev-code/itis-blocked-sites');
-        console.log('Errore:', error);
+        console.log('Log errore (utile per riportare il problema):', error);
       });
+}
+fetchData();
+setInterval(fetchData,15000);
